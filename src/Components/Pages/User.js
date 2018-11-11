6f343f1d5ac4488715dc5aea8f07fae52b5d79ce
jakir <jakir.hosan@apsissolutions.com>
2018-11-11 19:00:33 +0600

    get user

--- src/Components/Pages/User.js
+++ src/Components/Pages/User.js
@@ -6,28 +6,41 @@ class User extends Component {
   constructor (props){
     super(props)
     this.state = {
+      isLoading:false,
       userId:[]
     }
   }
   
 
   componentDidMount(){
-    fetch('http://192.168.0.37/apsisengine/id-test')
+    fetch('http://43.224.117.211/uro-cola2/test-url')
       .then(res=>res.json())
       .then(data=>{
         this.setState({
+          isLoading:true,
           userId:data
         })
+        //console.log(data);
       })
   }
 
   
     render() {
-      return (
-          <div className="container">
-            <h1>Hello User</h1>
-          </div>
-      );
+      var {isLoading,userId} = this.state;
+
+      if(!isLoading)
+      {
+        return <div>Loading....</div>
+      }
+      else
+      {
+        return (<h1> 
+              {
+                this.state.userId.map((x, i) => 
+                  <li key={i}>{x.id}</li>)
+              }
+            </h1>);
+      }
     }
   }
 
